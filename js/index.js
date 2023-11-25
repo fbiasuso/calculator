8e57e3d5660e7aa8a5220d02fa4638a7669320e5
Francisco Biasuso <fbiasuso@gmail.com>
2023-11-25 07:23:09 -0300

    Added features to the main buttons

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-
+/* 
 
 class Calculator {
 
@@ -47,6 +47,96 @@ class Calculator {
         return this.num1 % this.num2 
     }
 }
+ */
+
+let screen = document.getElementById("screen");
+let operand = 0;
+let operation = "";
+
+document.addEventListener("click", function(event)
+{
+     /* (operation === "result") && (screen.value = ""); */
+    
+    if(event.target.value && event.target.id !== "screen" && event.target.id !== "display"){
+       
+        switch (event.target.value) {
+            
+            case "addition":
+                operation = "+";   
+                operand = Number(screen.value); 
+                screen.value = "";   
+            break;
+            case "subtraction":
+                operation = "-";
+                operand = Number(screen.value); 
+                screen.value = "";   
+                
+            break;
+            case "multiplication":
+                operation = "x";
+                operand = Number(screen.value); 
+                screen.value = "";   
+                
+            break;
+            case "division":
+                operation = "÷";
+                operand = Number(screen.value); 
+                screen.value = "";   
+                
+            break;
+            case "back":
+                screen.value = screen.value.slice(0, -1); 
+
+            break;
+            case "result":
+            /* 
+            let separarEnTerminosSignoMas = screen.value.split("+");
+            console.log("Más: ",separarEnTerminosSignoMas);
+            
+            let separarEnTerminosSignoMenos = separarEnTerminosSignoMas.map(op=>op.split("-"));
+            console.log("Menos: ",separarEnTerminosSignoMenos);
+            let element = "";
+             let separarEnTerminosSignomult = separarEnTerminosSignoMenos.map ((o)=>{ for (let index = 0; index < o.length; index++) {
+                console.log("--> ",o[index])
+                            if (o[index] === "x" || o[index] === "÷") {
+                                
+                                element = o[index] === "x" ? Number(element) * Number(o[index-1]) : Number(element) / Number(o[index-1])
+                                console.log("--D ",elemen)
+                            } else {
+                                        element = `${element}${o[index]}`;
+
+                                        
+                                    }
+                                    return o[index] = element; 
+
+            }        })
+
+            console.log(element);
+            console.log(separarEnTerminosSignomult);  */
+
+            screen.value = (operand && operation === "+") ? operand + Number(screen.value)
+                            : (operand && operation === "-") ? operand - Number(screen.value)
+                            : (operand && operation === "x") ? operand * Number(screen.value)
+                            : (operand && operation === "÷") ? operand / Number(screen.value)
+                            : "Operador no válido";
+            operation = "result";
+
+            break;
+            case "clear":
+                screen.value = "";
+            break;
+            default:
+                screen.value +=  event.target.value;
+            break;
+        }
+       
+       
+        
+    }
+    
+});
+
+
 
 /* const num1 = Number(prompt("Escriba el primer número, será usado como el primer operando: "));
 const num2 = Number(prompt("Escriba el segundo número, será usado como el segundo operando y como la base de la potencia o raíz: "));
